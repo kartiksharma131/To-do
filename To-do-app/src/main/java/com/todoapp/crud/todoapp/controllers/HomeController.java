ad2be372a61345f4d1bdae0db143233203f481e3
kartiksharma131 <51881502+kartiksharma131@users.noreply.github.com>
2026-05-06 21:57:23 +0100

    template creation

--- To-do-app/src/main/java/com/todoapp/crud/todoapp/controllers/HomeController.java
+++ To-do-app/src/main/java/com/todoapp/crud/todoapp/controllers/HomeController.java
@@ -1,18 +1,15 @@
 package com.todoapp.crud.todoapp.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.List;
 
-@RestController
+
+@Controller
 public class HomeController {
 
     @RequestMapping("/todos")
-    public List<String> getTodos(){
-        List<String> todo = Arrays.asList("Go to the gym", "Buy groceries", "Finish homework");
-        return todo;
+    public String getTodos(){
+        return "todos";
     }
 }
